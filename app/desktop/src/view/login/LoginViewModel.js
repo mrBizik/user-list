16f08630c0aa42cd041becccdee802aa8a8f6e3f
mrBizik <bizigg@gmail.com>
2021-09-02 22:36:14 +0500

    first commit

---
+++ app/desktop/src/view/login/LoginViewModel.js
@@ -0,0 +1,7 @@
+Ext.define('UserList.view.login.LoginViewModel', {
+	extend: 'Ext.app.ViewModel',
+	alias: 'viewmodel.loginviewmodel',
+	data: {
+		name: 'UserList'
+	}
+});
---
+++ app/desktop/src/view/personnel/PersonnelViewModel.js
@@ -0,0 +1,7 @@
+Ext.define('UserList.view.personnel.PersonnelViewModel', {
+	extend: 'Ext.app.ViewModel',
+	alias: 'viewmodel.personnelviewmodel',
+	data: {
+		name: 'UserList'
+	}
+});
